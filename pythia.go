66684da376bfa3420b430e3f459597baac55b411
benjaminwyss <38589542+benjaminwyss@users.noreply.github.com>
2020-09-01 10:33:49 -0500

    implement zero knowledge proofs

--- pythia.go
+++ pythia.go
@@ -92,3 +92,79 @@ func ApplyUpdateToken(xHash []byte, updateToken *big.Int) []byte {
 	return gt.Marshal()
 
 }
+
+func GenerateProof(blindHash []byte, saltHash []byte, kw []byte, blindResult []byte) ([]byte, *big.Int, *big.Int) {
+	k1 := new(big.Int).SetBytes(blindHash)
+	k2 := new(big.Int).SetBytes(saltHash)
+
+	kwInt := new(big.Int).SetBytes(kw)
+
+	beta := BilinearPairing(k1, k2)
+
+	pQ := new(bn256.G1).ScalarBaseMult(kwInt)
+	p := pQ.Marshal()
+
+	v, _ := rand.Int(rand.Reader, bn256.Order)
+
+	t1Q := new(bn256.G1).ScalarBaseMult(v)
+	t1 := t1Q.Marshal()
+
+	t2Gt := BilinearPairing(k1, k2)
+	t2Gt.ScalarMult(t2Gt, v)
+	t2 := t2Gt.Marshal()
+
+	hash := sha256.New()
+	hash.Write(p)
+	hash.Write(beta.Marshal())
+	hash.Write(blindResult)
+	hash.Write(t1)
+	hash.Write(t2)
+
+	c := hash.Sum(nil)
+
+	cInt := new(big.Int).SetBytes(c)
+
+	u := new(big.Int)
+	u.Mul(cInt, kwInt)
+	u.Sub(v, u)
+	u.Mod(u, bn256.Order)
+
+	return p, cInt, u
+
+}
+
+func VerifyProof(blindHash []byte, salt string, blindResult []byte, p []byte, c *big.Int, u *big.Int) bool {
+	k1 := new(big.Int).SetBytes(blindHash)
+
+	saltHash := sha256.Sum256([]byte(salt))
+	k2 := new(big.Int).SetBytes(saltHash[:])
+
+	beta := BilinearPairing(k1, k2)
+
+	uQ := new(bn256.G1).ScalarBaseMult(u)
+	pc := new(bn256.G1)
+	pc.Unmarshal(p)
+	pc.ScalarMult(pc, c)
+
+	t1 := new(bn256.G1).Add(uQ, pc).Marshal()
+
+	betau := BilinearPairing(k1, k2)
+	betau.ScalarMult(betau, u)
+	yc := new(bn256.GT)
+	yc.Unmarshal(blindResult)
+	yc.ScalarMult(yc, c)
+
+	t2 := new(bn256.GT).Add(betau, yc).Marshal()
+
+	hash := sha256.New()
+	hash.Write(p)
+	hash.Write(beta.Marshal())
+	hash.Write(blindResult)
+	hash.Write(t1)
+	hash.Write(t2)
+
+	cPrime := hash.Sum(nil)
+	cPrimeInt := new(big.Int).SetBytes(cPrime)
+
+	return c.Cmp(cPrimeInt) == 0
+}
